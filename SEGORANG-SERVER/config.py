312a6c1578ffedac36232843f643f113ebee6532
Scof <32575460+songyw0517@users.noreply.github.com>
2023-02-08 06:50:37 +0900

    update language

--- SEGORANG-SERVER/config.py
+++ SEGORANG-SERVER/config.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 
 load_dotenv(verbose=True)
 
-APP_NAME = "SESCO"
+APP_NAME = "SEGORANG"
 BASEDIR = os.path.abspath(os.path.dirname(__file__))
 FLASK_CONFIG = os.getenv('FLASK_CONFIG') or 'development'
 
@@ -36,9 +36,6 @@ class Config:
     ADMIN_ID = os.environ[APP_NAME + '_ADMIN_ID']
     ADMIN_PW = os.environ[APP_NAME + '_ADMIN_PW']
 
-    # AI
-    AI_SERVER_DOMAIN = os.environ[APP_NAME + '_AI_SERVER_DOMAIN']
-    AI_SERVER_API_KEY = os.environ[APP_NAME + '_AI_SERVER_API_KEY']
 
     @staticmethod
     def init_app(app):
@@ -49,19 +46,7 @@ if FLASK_CONFIG == 'development':
     class AppConfig(Config):
         DEBUG = True
         TESTING = False
-        JWT_ACCESS_TOKEN_EXPIRES = 60 * 60 * 24 * 2
-        JWT_REFRESH_TOKEN_EXPIRES = 60 * 60 * 24 * 60
 
-elif FLASK_CONFIG == 'production':
-    class AppConfig(Config):
-        DEBUG = False
-        TESTING = False
-        SECRET_KEY = os.environ[APP_NAME + "_SECRET_KEY"]
-
-        # Google Oauth
-        GOOGLE_OAUTH_CLIENT_ID = os.environ['GOOGLE_OAUTH_CLIENT_ID']
-        GOOGLE_OAUTH_CLIENT_SECRET = os.environ['GOOGLE_OAUTH_CLIENT_SECRET']
-        GOOGLE_OAUTH_REDIRECT_URI = os.environ['GOOGLE_OAUTH_REDIRECT_URI']
 else:
     raise Exception("Flask Config not Selected.")
 
