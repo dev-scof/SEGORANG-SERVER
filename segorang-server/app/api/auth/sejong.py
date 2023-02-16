ea69d0b935a6a26ead36cee0a1278a518d4efc9d
Scof <32575460+songyw0517@users.noreply.github.com>
2023-02-16 12:04:32 +0900

    update: sejgon auth api

--- segorang-server/app/api/auth/sejong.py
+++ segorang-server/app/api/auth/sejong.py
@@ -4,6 +4,7 @@ from flask_validation_extended import Validator, Json, MinLen
 from app.api.response import bad_request, response_200, unauthorized
 from app.api.decorator import timer
 from controller.auth.sejong import sejong_auth
+from model.mysql.user import User
 from config import Config
 from datetime import timedelta
 from . import api_auth as api
@@ -16,12 +17,24 @@ def sejong_auth_api(
     sj_id=Json(str),
     sj_pw=Json(str)
 ):
-    cur_data = request.get_json()
-    result = sejong_auth(cur_data)
+    result = sejong_auth({
+        'sj_id':sj_id,
+        'sj_pw':sj_pw
+    })
+
     if result.is_auth == True:
         current_app.logger.info("인증 완료")
-        return response_200({
-            'AuthResponse': result,
-        })
+        user_model = User(current_app.db)
+        model_res = user_model.get_user_by_single_property('sj_id', sj_id)
+        if model_res is None:
+            return response_200({
+                'AuthResponse': result,
+                'in_db': False
+            })
+        else:
+            return response_200({
+                'AuthResponse': result,
+                'in_db': True
+            })
     else:
         return bad_request('잘못된 정보입니다.')
