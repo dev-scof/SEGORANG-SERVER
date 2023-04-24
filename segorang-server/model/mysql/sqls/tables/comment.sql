640a9b9a66804109cf7dd3117ba78e719e115a2a
Scof <32575460+songyw0517@users.noreply.github.com>
2023-04-24 12:38:29 +0900

    Fix: change typo

--- segorang-server/model/mysql/sqls/tables/comment.sql
+++ segorang-server/model/mysql/sqls/tables/comment.sql
@@ -4,7 +4,7 @@ CREATE TABLE `comment` (
 	`content`	            VARCHAR(300)	NOT NULL,
 	`created_at`	        TIMESTAMP	    NOT NULL	DEFAULT CURRENT_TIMESTAMP,
 	`updated_at`	        TIMESTAMP	    NOT NULL	DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-	`parrent_comment_id`	INTEGER     	NOT NULL	DEFAULT 0,
+	`parent_comment_id`	INTEGER     	NOT NULL	DEFAULT 0,
 	`user_id`	            VARCHAR(20)	    NOT NULL,
 	`post_id`	            INTEGER	NOT     NULL,
 	`is_deleted`			BOOLEAN			NOT NULL	DEFAULT false
