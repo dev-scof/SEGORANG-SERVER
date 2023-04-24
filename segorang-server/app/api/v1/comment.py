9ac76baf2eb1051bb1d1862f793ef55939a7e19c
Scof <32575460+songyw0517@users.noreply.github.com>
2023-04-24 19:46:02 +0900

    Feat: 댓글 좋아요 APIs 구현

--- segorang-server/app/api/v1/comment.py
+++ segorang-server/app/api/v1/comment.py
@@ -13,6 +13,7 @@ from datetime import timedelta
 from config import Config
 from . import api_v1 as api
 from model.mysql.comment import Comment
+from model.mysql.comment_like import Comment_Like
 from model.mysql.post import Post
 from model.mysql.post_image import Post_Image
 from model.mysql.post_like import Post_Like
@@ -100,4 +101,50 @@ def comment_get_api(
     '''
     comment_model = Comment(current_app.db)
     model_res = comment_model.get_comment_by_postid(post_id)
+    return response_200(model_res)
+
+@api.post('/comment/like/<int:comment_id>')
+@timer
+@login_required
+@Validator(bad_request)
+def comment_like_insert_api(
+    comment_id=Route(int, rules=Min(0))
+):
+    '''
+    댓글 좋아요 생성 API
+    '''
+    model=Comment_Like(current_app.db)
+    model_res = model.insert_like(g.user_id, comment_id)
+
+    if isinstance(model_res, Exception):
+        return conflict(model_res.__str__())
+    else:
+        return response_200()
+
+
+@api.delete('/comment/like/<int:comment_id>')
+@timer
+@login_required
+@Validator(bad_request)
+def comment_like_delete_api(
+    comment_id=Route(int, rules=Min(0))
+):
+    """ 댓글 좋아요 삭제 API """
+    model = Comment_Like(current_app.db)
+    model_res = model.delete_like(g.user_id, comment_id)
+    if isinstance(model_res, Exception):
+        return bad_request(model_res.__str__())
+    else:
+        return no_content
+
+@api.get('/comment/like/<int:comment_id>')
+@timer
+@login_required
+@Validator(bad_request)
+def comment_like_cnt_get_api(
+    comment_id=Route(int, rules=Min(0))
+):
+    """ 게시물 좋아요 수 반환 API"""
+    model = Comment_Like(current_app.db)
+    model_res = model.get_like_cnt(comment_id)
     return response_200(model_res)
