9c859ce153ca109e496dd4b98552aad3a12aa186
Scof <32575460+songyw0517@users.noreply.github.com>
2023-04-25 15:54:04 +0900

    Feat: youtube APIs 구현 ... 2

--- segorang-server/model/mysql/youtube.py
+++ segorang-server/model/mysql/youtube.py
@@ -16,10 +16,13 @@ class Youtube(Model):
         keys, values = get_fields_data(data)
         query = self.insert_query.format(
             table_name=self.table_name,
-            keys=', '.join(map(str, keys)),
+            keys=', '.join(map(lambda x:str(x), keys)),
             values=', '.join(map(set_quote, values))
         )
         try:
+            self.cursor.execute('SET NAMES utf8mb4')
+            self.cursor.execute("SET CHARACTER SET utf8mb4")
+            self.cursor.execute("SET character_set_connection=utf8mb4")
             self.cursor.execute(query)
             self.conn.commit()
             return True
@@ -44,3 +47,34 @@ class Youtube(Model):
             return None
         else:
             return list(map(lambda x:dict(zip(keys, x)),data))
+        
+    def update_youtube_by_id(self, id:int, data:dict):
+        keys, values = get_fields_data(data)
+        query = self.update_query.format(
+            table_name=self.table_name,
+            update_data=', '.join(map(lambda x,y:x+'='+set_quote(y), keys, values)),
+            condition=f'WHERE id={id}'
+        )
+        print(query)
+        try:
+            self.cursor.execute(query)
+            self.conn.commit()
+            return True
+        except Exception as e:
+            return e
+        finally:
+            self.cursor.close()
+    
+    def delete_youtube_api(self, id:int):
+        query = self.delete_query.format(
+            table_name=self.table_name,
+            condition=f'WHERE id={id}'
+        )
+        try:
+            self.cursor.execute(query)
+            self.conn.commit()
+            return True
+        except Exception as e:
+            return e
+        finally:
+            self.cursor.close()
