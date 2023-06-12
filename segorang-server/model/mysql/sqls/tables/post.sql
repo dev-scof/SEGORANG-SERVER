ab68d73d2a555f7cb4c8185c5741ac5f08773364
Scof <32575460+songyw0517@users.noreply.github.com>
2023-06-13 01:27:11 +0900

    Revert "Style: Rename Post Column view_cnt to view_num"
    This reverts commit fae88a862c66d1e69fba90f2ed68f975f4d02074.

--- segorang-server/model/mysql/sqls/tables/post.sql
+++ segorang-server/model/mysql/sqls/tables/post.sql
@@ -4,7 +4,7 @@ CREATE TABLE `post` (
 	`id`	        INTEGER	        NOT NULL    AUTO_INCREMENT      PRIMARY KEY,
 	`title`	        VARCHAR(100)    NOT NULL,
 	`content`	    VARCHAR(2000)	NOT NULL,
-	`view_num`	    INTEGER	        NOT NULL	DEFAULT 0,
+	`view_cnt`	    INTEGER	        NOT NULL	DEFAULT 0,
 	`category`	    VARCHAR(10)	    NULL	    DEFAULT NULL,
 	`created_at`	TIMESTAMP	    NOT NULL	DEFAULT CURRENT_TIMESTAMP,
 	`updated_at`	TIMESTAMP	    NOT NULL	DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
